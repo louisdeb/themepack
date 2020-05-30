a942b75335f39e2d59a4456704df33a628bb26a3
Louis de Beaumont <debeaumont.louis@gmail.com>
2020-05-30 13:19:47 +0100

    Show webview on 'home' command

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,100 @@
 'use strict';
 
+import * as path from 'path';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('themepack.helloWorld', () => {
-		vscode.window.showInformationMessage('themepack!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('themepack.home', () => {
+			HomePanel.createOrShow(context.extensionPath);
+		})
+	);
 }
 
 export function deactivate() {}
+
+class HomePanel {
+	public static currentPanel: HomePanel | undefined; // singleton
+
+	public static readonly viewType = 'home';
+
+	private readonly _panel: vscode.WebviewPanel;
+	private readonly _extensionPath: string;
+	private _disposables: vscode.Disposable[] = [];
+
+	public static createOrShow(extensionPath: string) {
+		const column = vscode.window.activeTextEditor
+						? vscode.window.activeTextEditor.viewColumn
+						: undefined;
+
+		if (HomePanel.currentPanel) {
+			HomePanel.currentPanel._panel.reveal(column);
+			return;
+		}
+
+		const panel = vscode.window.createWebviewPanel(
+			HomePanel.viewType,
+			'Home',
+			column || vscode.ViewColumn.One,
+			{
+				enableScripts: true,
+
+				// restricts resource access
+				localResourceRoots: [vscode.Uri.file(path.join(extensionPath, 'media'))]
+			}
+		);
+
+		HomePanel.currentPanel = new HomePanel(panel, extensionPath);
+	}
+
+	public static revive(panel: vscode.WebviewPanel, extensionPath: string) {
+		HomePanel.currentPanel = new HomePanel(panel, extensionPath);
+	}
+
+	private constructor(panel: vscode.WebviewPanel, extensionPath: string) {
+		this._panel = panel;
+		this._extensionPath = extensionPath;
+
+		this._update();
+
+		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+		this._panel.onDidChangeViewState(
+			e => {
+				if (this._panel.visible)
+					this._update();
+			},
+			null,
+			this._disposables
+		);
+
+		this._panel.webview.onDidReceiveMessage(
+			message => {
+				switch (message.command) {
+					case 'alert': 
+						vscode.window.showErrorMessage(message.text);
+						return;
+				}
+			}
+		);
+	}
+
+	public dispose() {
+		HomePanel.currentPanel = undefined;
+		this._panel.dispose();
+
+		while (this._disposables.length) {
+			const x = this._disposables.pop();
+			if (x)
+				x.dispose();
+		}
+	}
+
+	private _update() {
+		const webview = this._panel.webview;
+
+		this._panel.title = 'collection';
+		this._panel.webview.html = `<!doctype html><html><body>testbody</body></html>`;
+	}
+}
