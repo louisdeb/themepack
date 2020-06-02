b2faab400f0dd11613992dba13d7ff058fbe2d6e
Louis de Beaumont <debeaumont.louis@gmail.com>
2020-06-02 10:15:58 +0100

    Clean using 'forwards' fill on animations

--- res/js/main.js
+++ res/js/main.js
@@ -1,24 +1,23 @@
 var collection = [];
 
 function fixit() {
-  if (collection.length != 0)
-    return;
+  if (collection.length) return;
 
   var supplydrop = document.getElementById('supplydrop');
   supplydrop.classList.add('swing-animation');
-  supplydrop.style.top = '21.3%';
-  setTimeout(function() { // on animation end
+
+  setTimeout(function() {
     supplydrop.style.cursor = 'pointer';
     supplydrop.onclick = function () {
       var crateOpenOverlay = document.getElementById('crate-open-overlay');
       crateOpenOverlay.classList.add('fade-in');
       crateOpenOverlay.style.display = 'flex';
-      crateOpenOverlay.style.opacity = 0.9;
       
       supplydrop.classList.add('fade-out');
+
       setTimeout(function() {
         supplydrop.parentNode.removeChild(supplydrop);
-      }, 999);
+      }, 1000);
 
       const spinner = new Spinner(document.getElementById('spinner'));
     }
@@ -26,11 +25,11 @@ function fixit() {
 
   var nothemes = document.getElementById('nothemes');
   nothemes.classList.add('fade-out');
+
   setTimeout(function() {
     nothemes.parentNode.removeChild(nothemes);
-  }, 999);
+  }, 1000);
 }
-
 
 class Spinner {
   constructor(domElement) {
